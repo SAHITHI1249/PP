5e35c109ad46c80a2d47070b7a5c3067aea4a0a4
SAHITHI1249 <121919213+SAHITHI1249@users.noreply.github.com>
2023-04-28 12:30:58 +0530

    Add files via upload

---
+++ lowup.py
@@ -0,0 +1,9 @@
+a = input("enter a charecter:")
+if (a>='A' and a<='Z'):
+    print("Upper case")
+elif (a>='a' and a<='z'):
+    print("lower case")
+elif (a>='0' and a<='9'):
+    print("digit")
+else:
+    print("speacial charecter")
