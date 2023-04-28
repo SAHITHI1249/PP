c36c97518b72c43d787484af4da7545625f92604
SAHITHI1249 <121919213+SAHITHI1249@users.noreply.github.com>
2023-04-28 11:44:05 +0530

    Add files via upload

---
+++ details.py
@@ -0,0 +1,10 @@
+name = input("enter name:")
+ad = input("enter address:")
+email = input("enter email")
+pn =input("enter phone number:")
+print("Name : {}".format(name))
+print("Adress : {}".format(ad))
+print("Email : {}".format(email))
+print("Phone number : {}".format(pn))
+
+
