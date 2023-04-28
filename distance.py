7eace64370b66fe4383327db899978bfdfa4312d
SAHITHI1249 <121919213+SAHITHI1249@users.noreply.github.com>
2023-04-28 11:43:53 +0530

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,8 @@
+import math
+x1 = int(input("enter x1"))
+y1 = int(input("enter y1"))
+x2 = int(input("enter x2"))
+y2 = int(input("enter y2"))
+d = pow((pow((x2-x1),2)+pow((y2-y1),2)),0.5)
+print("DISTANCE IS d :",d)
+
