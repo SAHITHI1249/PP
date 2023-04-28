38bd8e5976dfd4096ab3d85cfa310c2caa2ed0f7
SAHITHI1249 <121919213+SAHITHI1249@users.noreply.github.com>
2023-04-28 11:44:23 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,9 @@
+a=5
+b=5
+print("Sum : {}".format(a+b))
+print("Product : {}".format(a*b))
+print("Difference : {}".format(a-b))
+print("Quotient : {}".format(a/b)) 
+print("Another method")
+print("Sum is {},  Product is {},  DIfference is {},  Quotient is {},".format(a+b,a*b,a-b,a/b))
+
